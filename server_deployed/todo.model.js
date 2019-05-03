cc1c9afb108ffd595717e9dfd2fc983f0573fff7
zhagm <zhagdev@gmail.com>
2019-05-03 16:33:59 -0700

    fixes? not sure about last changes but pushing to pull from h2s monitor

--- server_deployed/todo.model.js
+++ server_deployed/todo.model.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let Todo = new Schema({
-  task: {
-    type: String,
+  task: { 
+    type: String, 
     required: true
   },
-  completed: {
-    type: Boolean,
-    required: true
+  completed: { 
+    type: Boolean, 
+    required: true 
   }
 });
 
